e112b55030c4f442418a533d4a87a4c7af3cb369
moncefoukal <drglemnyt@gmail.com>
2022-01-08 09:39:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let user="username";
+let pw="123";
+function accee(login,pw){
+    //condition if
+    if(login=="username"&& pw=="123"){
+        return true;
+    }
+    else{
+        return false;
+    
+    }
+}
+
+//appel function
+console.log(accee(user,pw));
+function somme(a,b){
+    return(a+b)
+}
+let num1 = 20
+let num2 =30
+console.log(somme (num1,num2));
